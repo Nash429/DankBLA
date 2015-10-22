12734c50cf95fba12761e4b103926f1901aed4fb
Nash429 <noah.the.buttkickingest@gmail.com>
2015-10-21 19:22:27 -0700

    first class

---
+++ mine.cpp
@@ -0,0 +1,21 @@
+//     ===========================
+//        Welcome to Pocket C++
+//     ===========================
+//
+//     You can compile C++ files using F9 key (try it!).
+//     Then you can execute the generated program pressing Ctrl+F9.
+//
+//     Good luck!
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world";
+	system("pause");
+	return 0;
+}
